2773be7fd7cff268de4c590034771ca31b398ce1
Grant Keith <grant.keith@mail.mcgill.ca>
2020-05-16 21:13:47 -0400

    commit

--- APIs/Mailchimp API/app.js
+++ APIs/Mailchimp API/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,11 +17,56 @@ app.post("/", function(req, res){
     var lName = req.body.lName;
     var email = req.body.email;
 
-    console.log(fName);
-    console.log(lName);
-    console.log(email);
+    var data = {
+        members:[{
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: fName,
+                LNAME: lName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    console.log(jsonData);
+
+    const url = "https://us18.api.mailchimp.com/3.0/lists/de4df286a6";
+
+    const options = {
+        method:"POST",
+        auth: "keithggk23:6f1e11732578437a17150d47224cc74f-us18"
+    }
+    
+    const request = https.request(url, options, function(response){
+        if(response.statusCode==200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    console.log(request);
+
+    request.write(jsonData);
+    request.end();
 });
 
-app.listen(3000, function(){
+app.post("/failure", function (req, res){
+    res.redirect(__dirname)
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
+
+//API key
+//6f1e11732578437a17150d47224cc74f-us18
+
+//List ID
+//de4df286a6
--- intro-to-express/server.js
+++ intro-to-express/server.js
@@ -6,7 +6,20 @@ const app = express();
 
 app.get("/", function(request, response){
     response.send("Hello");
-})
+});
+
+app.get("/contact", function(request, response){
+    response.send("Email me at: grant.keith@mail.mcgill.ca");
+});
+
+app.get("/about", function(request, response){
+    response.send("My name is Grant and I love video games ;)");
+});
+
+app.get("/hobbies", function(request, response){
+    response.send("Coffee, Code, and Beer");
+});
+
 
 app.listen(3000, function(){
     console.log("Server started on Port 3000");
